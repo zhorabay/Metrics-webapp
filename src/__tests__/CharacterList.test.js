2dd307ab616c71c29b188179a85e7b607b851ca0
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-10 13:15:38 +0300

    Add search field, Back button

--- src/__tests__/CharacterList.test.js
+++ src/__tests__/CharacterList.test.js
@@ -1,56 +1,18 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import getCharacters from 'redux-thunk';
+import store from '../redux/store';
 import CharacterList from '../components/CharacterList';
-import { setCharacters } from '../redux/comicsSlice';
 
-const middlewares = [getCharacters];
-const mockStore = configureStore(middlewares);
-const initialState = {
-  comics: {
-    characters: [
-      {
-        id: 1,
-        name: 'Character 1',
-        description: 'Description 1',
-        thumbnail: {
-          path: 'path1',
-          extension: 'ext1',
-        },
-      },
-      {
-        id: 2,
-        name: 'Character 2',
-        description: 'Description 2',
-        thumbnail: {
-          path: 'path2',
-          extension: 'ext2',
-        },
-      },
-    ],
-  },
-};
-
-const store = mockStore(initialState);
-
-test('CharacterList renders characters correctly', async () => {
-  store.dispatch(setCharacters(initialState.comics.characters));
-
-  render(
+test('CharacterList matches snapshot', () => {
+  const { asFragment } = render(
     <Provider store={store}>
-      <CharacterList />
+      <Router>
+        <CharacterList />
+      </Router>
     </Provider>,
   );
 
-  await waitFor(() => {
-    const characterNames = screen.getAllByText(/Character \d/);
-    const characterDescriptions = screen.getAllByText(/Description \d/);
-    const characterImages = screen.getAllByAltText(/Character \d/);
-
-    expect(characterNames.length).toBe(2);
-    expect(characterDescriptions.length).toBe(2);
-    expect(characterImages.length).toBe(2);
-  });
+  expect(asFragment()).toMatchSnapshot();
 });
--- src/__tests__/HomePage.test.js
+++ src/__tests__/HomePage.test.js
@@ -1,8 +1,34 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../redux/store';
 import HomePage from '../components/HomePage';
 
-test('HomePage renders correctly', () => {
-  const { container } = render(<HomePage />);
-  expect(container).toMatchSnapshot();
+test('HomePage matches snapshot', () => {
+  const { asFragment } = render(
+    <Provider store={store}>
+      <Router>
+        <HomePage />
+      </Router>
+    </Provider>,
+  );
+
+  expect(asFragment()).toMatchSnapshot();
+});
+
+test('Search input changes value correctly', () => {
+  const { getByPlaceholderText } = render(
+    <Provider store={store}>
+      <Router>
+        <HomePage />
+      </Router>
+    </Provider>,
+  );
+
+  const searchInput = getByPlaceholderText('Search by category');
+
+  fireEvent.change(searchInput, { target: { value: 'Characters' } });
+
+  expect(searchInput.value).toBe('Characters');
 });
