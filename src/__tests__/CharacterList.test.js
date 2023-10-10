ffbfdac2579607345d4d6a916af9015f104b9e88
Assem Zhorabay <assem.zhorabay@gmail.com>
2023-10-10 11:11:27 +0300

    Finish testing

--- src/__tests__/CharacterList.test.js
+++ src/__tests__/CharacterList.test.js
@@ -1,14 +1,56 @@
 import React from 'react';
-import { render, waitFor, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import getCharacters from 'redux-thunk';
 import CharacterList from '../components/CharacterList';
+import { setCharacters } from '../redux/comicsSlice';
 
-test('CharacterList renders and matches snapshot', async () => {
-  render(<CharacterList />);
+const middlewares = [getCharacters];
+const mockStore = configureStore(middlewares);
+const initialState = {
+  comics: {
+    characters: [
+      {
+        id: 1,
+        name: 'Character 1',
+        description: 'Description 1',
+        thumbnail: {
+          path: 'path1',
+          extension: 'ext1',
+        },
+      },
+      {
+        id: 2,
+        name: 'Character 2',
+        description: 'Description 2',
+        thumbnail: {
+          path: 'path2',
+          extension: 'ext2',
+        },
+      },
+    ],
+  },
+};
+
+const store = mockStore(initialState);
+
+test('CharacterList renders characters correctly', async () => {
+  store.dispatch(setCharacters(initialState.comics.characters));
+
+  render(
+    <Provider store={store}>
+      <CharacterList />
+    </Provider>
+  );
 
   await waitFor(() => {
-    expect(screen.getByText('Marvel Characters')).toBeInTheDocument();
+    const characterNames = screen.getAllByText(/Character \d/);
+    const characterDescriptions = screen.getAllByText(/Description \d/);
+    const characterImages = screen.getAllByAltText(/Character \d/);
 
-    const { asFragment } = screen;
-    expect(asFragment()).toMatchSnapshot();
+    expect(characterNames.length).toBe(2);
+    expect(characterDescriptions.length).toBe(2);
+    expect(characterImages.length).toBe(2);
   });
 });
